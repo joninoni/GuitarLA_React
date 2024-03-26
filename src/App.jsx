680585f92e599a0fa7d8257e1fb3af299759c039
joninoni <jonibeltranhayer@gmail.com>
2024-03-26 11:38:36 -0600

    creo el hook de Cart y pasando la logica del app.jsx al hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,84 +1,10 @@
-import { useEffect, useState } from "react"
 import Guitar from "./Components/Guitar"
 import Header from "./Components/Header"
-import {db} from "./data/db.js"
+import useCart from "./hooks/useCart.js"
 
 function App() {
-
-    const initialCart= () =>{
-        const localStorageCart = localStorage.getItem("cart")
-        return localStorageCart ? JSON.parse(localStorageCart) : []
-    }
-
-    const [data,setData] =useState(db)
-    const [cart,setCart] = useState(initialCart)
-
-    useEffect( ()=>{
-        localStorage.setItem("cart",JSON.stringify(cart))
-    },[cart])
-
-    const MAX_ITEMS = 5
-    const MIN_ITEMS = 1
-
-    function addToCart(item){
-        //verificar si una guitarra ya existe
-        const itemExists=cart.findIndex( guitar => guitar.id === item.id)
-        if(itemExists >=0){
-            console.log("ya existe")
-            if(cart[itemExists].quantity >= MAX_ITEMS) return
-            const updateCart=[...cart]
-            updateCart[itemExists].quantity++
-            setCart(updateCart)
-        }
-        else{
-            console.log("No existe... agregando al carrito")
-            item.quantity = 1
-            setCart([...cart,item])
-            // setCart( prevCart => [...prevCart,item])
-        }
-    }
-
-    function removeFromCart(id){
-        // metodo 1 para eliminar un elemento del carrito
-        // const newCartItems = cart.filter(item => item.id !== id )
-        // setCart(newCartItems)
-        //metodo 2 para eliminar un elemento del carrito
-        setCart(prevCart => prevCart.filter( guitar => guitar.id !== id))
-    }
-
-    function increaseQuantity(id){
-
-        const updateCart = cart.map( item => {
-            if(item.id === id && item.quantity < MAX_ITEMS){
-                return{
-                    ...item,
-                    quantity: item.quantity + 1
-                }
-            }
-            return item
-        })
-
-        setCart(updateCart)
-    }
-
-    function decreaseQuantity(id){
-
-        const updateCart = cart.map( item => {
-            if(item.id === id && item.quantity > MIN_ITEMS) {
-                return {
-                    ...item,
-                    quantity : item.quantity - 1
-                }
-            }
-            return item                       
-        })
-
-        setCart(updateCart)
-    }
-
-    function clearCart(){
-       setCart([])
-    }
+    //obteniendo los valores del hook de Cart
+    const {data,cart,addToCart,removeFromCart,increaseQuantity,decreaseQuantity,clearCart,} = useCart()
 
     return (
     <>
@@ -99,7 +25,7 @@ function App() {
                            <Guitar
                                 guitar={guitar}
                                 key={guitar.id}
-                                setCart={setCart}
+                                // setCart={setCart}
                                 addToCart={ addToCart}
                            />
                     ))}
